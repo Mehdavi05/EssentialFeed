85cb7ee4d9404c169b6be657f5fae2a6edb0cb73
Shujaat Hussain <shujaat.apple05@gmail.com>
2022-08-24 19:20:01 +0400

    Create essential feed framework

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Shujaat-MacBook on 8/24/22.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
