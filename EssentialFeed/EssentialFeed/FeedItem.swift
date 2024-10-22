b077b824294137491d7f13429ded177b97488c0f
Shujaat Hussain <shujaat.apple05@gmail.com>
2024-10-22 20:20:59 +0400

    Create the FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Shujaat-MacBook on 10/22/24.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
