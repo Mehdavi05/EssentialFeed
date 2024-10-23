135db368c7910662cc18ff0df4b444bd38b3a614
Shujaat Hussain <shujaat.apple05@gmail.com>
2024-10-23 20:42:55 +0400

    Simplify type declarations with a helper function

--- EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/RemoteFeedLoaderTests.swift
@@ -18,7 +18,6 @@ class RemoteFeedLoaderTests : XCTestCase {
     
     func test_load_requestsDataFromURL() {
         let url = URL(string: "https://any-url.com")!
-        
         let (sut, client) = makeSUT(url: url)
         
         sut.load { _ in }
@@ -28,7 +27,6 @@ class RemoteFeedLoaderTests : XCTestCase {
     
     func test_loadTwice_requestsDataFromURLTwice() {
         let url = URL(string: "https://any-url.com")!
-        
         let (sut, client) = makeSUT(url: url)
         
         sut.load() { _ in }
@@ -40,7 +38,7 @@ class RemoteFeedLoaderTests : XCTestCase {
     func test_load_deliversErrorOnClientError() {
         let (sut, client) = makeSUT()
         
-        expect(sut: sut, toCompleteWithResult: .failure(RemoteFeedLoader.Error.connectivity), when: {
+        expect(sut: sut, toCompleteWithResult: failure(.connectivity), when: {
             let clientError = NSError(domain: "Test", code: 0)
             client.complete(with: clientError)
         })
@@ -48,11 +46,10 @@ class RemoteFeedLoaderTests : XCTestCase {
     
     func test_load_deliversErrorOnNon200HTTPResponse() {
         let (sut, client) = makeSUT()
-        
         let samples = [199, 201, 300, 400, 500]
         
         samples.enumerated().forEach { index, code in
-            expect(sut: sut, toCompleteWithResult: .failure(RemoteFeedLoader.Error.invalidData), when: {
+            expect(sut: sut, toCompleteWithResult: failure(.invalidData), when: {
                 let json = makeItemsJSON([])
                 client.complete(withStatusCode: code, data: json, at: index)
             })
@@ -62,7 +59,7 @@ class RemoteFeedLoaderTests : XCTestCase {
     func test_load_deliversErrorOn200HTTPResponseWithInvalidJSON() {
         let (sut, client) = makeSUT()
         
-        expect(sut: sut, toCompleteWithResult: .failure(RemoteFeedLoader.Error.invalidData), when:{
+        expect(sut: sut, toCompleteWithResult: failure(.invalidData), when:{
             let invalidJson = Data("invalid json".utf8)
             client.complete(withStatusCode: 200, data: invalidJson)
         })
@@ -129,8 +126,11 @@ class RemoteFeedLoaderTests : XCTestCase {
         return (sut, client)
     }
     
-    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line)
-    {
+    private func failure(_ error: RemoteFeedLoader.Error) -> RemoteFeedLoader.Result {
+        return .failure(error)
+    }
+    
+    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
         addTeardownBlock { [weak instance] in
             XCTAssertNil(instance, "Instance should  have been deallocated. Potential memory leak.", file: file, line: line)
         }
