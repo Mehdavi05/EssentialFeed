cdc20aa199016a5c731a1c0686c27430113d5017
Shujaat Hussain <shujaat.apple05@gmail.com>
2024-10-23 08:16:11 +0400

    Delivers items array on 200 HTTP response with JSON items

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -30,8 +30,8 @@ public class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, _):
-                if let _ = try? JSONSerialization.jsonObject(with: data){
-                    completion(.success([]))
+                if let root = try? JSONDecoder().decode(Root.self, from: data){
+                    completion(.success(root.items))
                 } else {
                     completion(.failure(.invalidData))
                 }
@@ -40,6 +40,10 @@ public class RemoteFeedLoader {
                 completion(.failure(.connectivity))
             }
         }
+    }
+    
+    private struct Root: Decodable {
+        let items: [FeedItem]
     }
 }
 
