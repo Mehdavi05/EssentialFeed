46366ac94d716cfe8ab7baf981c8471fb5764091
Shujaat Hussain <shujaat.apple05@gmail.com>
2024-11-05 06:53:07 +0400

    Extract duplicate test helpers into a shared scope

---
+++ EssentialFeed/EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -0,0 +1,16 @@
+//
+//  SharedTestHelpers.swift
+//  EssentialFeedTests
+//
+//  Created by Shujaat-MacBook on 11/5/24.
+//
+
+import Foundation
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
+
+func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
